c68753fcfd48bf641caf7ffe93e98a61c7151272
roshan <roshan.5893@gmail.com>
2017-12-12 02:07:05 -0600

    Timing issue fixed

--- Humidity/dialog.cpp
+++ Humidity/dialog.cpp
@@ -10,56 +10,65 @@
 #include <QFile>
 #include <QCoreApplication>
 #include <QTextStream>
+#include <QString>
+#include <iostream>
+#include <fstream>
+#include<QtCore>
+
+using namespace std;
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
-{
+{//---------------------------------GLOBAL VARIABLES----------------------------------------------------------
     timer=new QTimer(this);
-
     ui->setupUi(this);
     ui->humidity_lcdNumber->display("-------");
     ui->humidity_lcdNumber1->display("-------");
-  //  connect(ui->start_pushbutton,SIGNAL(clicked(bool)),ui->time_lcdNumber,SLOT(display(double)))
-  //  ui->plot->graph(0)->setAdaptiveSampling(0);                                                     // this line of code crashes the program! dont know why!
+    ui->temperature_lcdNumber->display("-------");
     ui->plot->addGraph();
     ui->plotA->addGraph();
+    ui->plot_temp->addGraph();
     ui->plot->graph()->setPen(QPen(Qt::blue));
-    ui->plotA->graph(0)->setPen(QPen(Qt::red));
+    ui->plotA->graph()->setPen(QPen(Qt::red));
+    ui->plot_temp->graph()->setPen(QPen(Qt::green));
     ui->plot->xAxis->setLabel("Time(Seconds)");
     ui->plot->yAxis->setLabel("Relative Humidity(%)");
     ui->plotA->xAxis->setLabel("Time(Seconds)");
-    ui->plotA->yAxis->setLabel("Resistance");
+    ui->plotA->yAxis->setLabel("Temperature(*C)");
+    ui->plot_temp->xAxis->setLabel("Time(Seconds)");
+    ui->plot_temp->yAxis->setLabel("Resistance(ohms)");
     ui->plot->graph()->setScatterStyle(QCPScatterStyle::ssCircle);
     ui->plot->graph()->setLineStyle(QCPGraph::lsLine);
-    ui->plotA->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
-    ui->plotA->graph(0)->setLineStyle(QCPGraph::lsLine);
+    ui->plotA->graph()->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plotA->graph()->setLineStyle(QCPGraph::lsLine);
+    ui->plot_temp->graph()->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plot_temp->graph()->setLineStyle(QCPGraph::lsLine);
     ui->plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
     ui->plotA->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+    ui->plot_temp->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
     arduino =new QSerialPort(this);
     serialBuffer="";
-
-
-
-   qDebug()<<"Number of ports :"<<QSerialPortInfo::availablePorts().length()<<"\n";
-
-   foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
-       qDebug()<<"Description"<< serialPortInfo.description()<<"\n";
-       qDebug()<<"Vendor ID :"<< serialPortInfo.vendorIdentifier()<<"\n";
-       qDebug()<<"Product ID :"<< serialPortInfo.productIdentifier()<<"\n";
-       qDebug()<<"SerialNumber:"<< serialPortInfo.serialNumber()<<"\n";
-
-     }
-
-    is_port_opened=false;
-    arduino_is_available=false;
-
-
-    QObject::connect(ui->start_pushButton,SIGNAL(released()),this,SLOT(Configure_Port()));
-
+  if(!file.is_open())
+    {
+        qDebug()<<"File not Found,retry........";
 
     }
 
+   qDebug()<<"Number of ports :"<<QSerialPortInfo::availablePorts().length()<<"\n";
+       foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
+            qDebug()<<"Description"<< serialPortInfo.description()<<"\n";
+            qDebug()<<"Vendor ID :"<< serialPortInfo.vendorIdentifier()<<"\n";
+            qDebug()<<"Product ID :"<< serialPortInfo.productIdentifier()<<"\n";
+            qDebug()<<"SerialNumber:"<< serialPortInfo.serialNumber()<<"\n";
+        }
+        is_port_opened=false;
+        arduino_is_available=false;
+        QObject::connect(ui->start_pushButton,SIGNAL(released()),this,SLOT(Configure_Port()));
+        QObject::connect(timer,SIGNAL(timeout()),this,SLOT(updateLCD(QString sensor_reading,QString sensor_reading1,QString sensor_reading2)));
+
+    }
+//--------------------------------------------------------------------------------------------------------------------------------
 /*
    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
        if (serialPortInfo.hasProductIdentifier() && serialPortInfo.hasVendorIdentifier()){
@@ -87,89 +96,98 @@ Dialog::Dialog(QWidget *parent) :
    }
 */
 
-
+//--------------------------------------------------------------------------------------------------------------------------------
 
 Dialog::~Dialog()
 {
-
     if (arduino->isOpen()){
         arduino->close();
     }
      delete ui;
 }
+
 void Dialog::readSerial()
 {
-
-   QStringList buffersplit= serialBuffer.split(",");
-   if (buffersplit.length()<3){
-        serialData= arduino->readAll();
+    buffersplit= serialBuffer.split("\n");
+    if (buffersplit.length()<4){                                                                         // compare  the length to 4 since we are getting 3 different values (RH, Resistance, Temperature)
+       serialData= arduino->readAll();
         serialBuffer+= QString::fromStdString(serialData.toStdString());
       }else{
         qDebug()<<buffersplit << time;
-       if(i>1){
-        Dialog::updateLCD(buffersplit[1],buffersplit[0]);
 
-         time+=1;
+       if(i>1){
+           Dialog::updateLCD(buffersplit[0],buffersplit[1],buffersplit[2]);
 
         }
         serialBuffer="";
         i=2;
         }
-}
+    }
 
-void Dialog:: updateLCD(QString sensor_reading, QString sensor_reading1){
-        ui->humidity_lcdNumber->display(sensor_reading);
+void Dialog:: updateLCD(QString sensor_reading, QString sensor_reading1, QString sensor_reading2){
+    qDebug() << "size: " << buffersplit.size() << "\n";
+         ui->humidity_lcdNumber->display(sensor_reading);
          ui->humidity_lcdNumber1->display(sensor_reading1);
+         ui->temperature_lcdNumber->display(sensor_reading2);
+        time+=1;
          ui->time_lcdNumber->display(time);
-    add_Point_humidity(ui->time_lcdNumber->value(),ui->humidity_lcdNumber->value());
-    add_Point_resistance(ui->time_lcdNumber->value(),ui->humidity_lcdNumber1->value());
-
-
+         if(j==1){
+             file<<"TIME"<<"\t"<<"RH%"<<"\t"<<"TEMPERATURE"<<"\t"<<"RESISTANCE"<<endl;
+         }
+         j=2;
+         qv_time.append(ui->time_lcdNumber->value());
+         add_Point_humidity(ui->humidity_lcdNumber->value());
+         add_Point_resistance(ui->humidity_lcdNumber1->value());
+         add_Point_temperature(ui->temperature_lcdNumber->value());
+         file<<ui->time_lcdNumber->value()<<"\t";
+         file<<ui->humidity_lcdNumber->value()<<"\t";
+         file<<ui->humidity_lcdNumber1->value()<<"\t\t";
+         file<<ui->temperature_lcdNumber->value()<<endl;
     }
 
 
 void Dialog:: Plot_humidity(){
-
-    ui->plot->graph()->setData(qv_time,humidity);
+    ui->plot->graph()->addData(qv_time,humidity);
     ui->plot->replot();
     ui->plot->update();
     ui->plot->graph()->rescaleAxes();
-
-
 }
 
 void Dialog::Plot_resisitance(){
-    ui->plotA->graph(0)->setData(qv_time,resistance);
+    ui->plotA->graph()->addData(qv_time,resistance);
     ui->plotA->replot();
     ui->plotA->update();
-    ui->plotA->graph(0)->rescaleAxes();
+    ui->plotA->graph()->rescaleAxes();
 }
 
-void Dialog:: add_Point_humidity(double time,double hum){
+void Dialog::Plot_temperature(){
+    ui->plot_temp->graph()->addData(qv_time,temperature);
+    ui->plot_temp->replot();
+   // qDebug()<<qv_time;
+    ui->plot_temp->update();
+    ui->plot_temp->graph()->rescaleAxes();
+
+}
+
+void Dialog:: add_Point_humidity(double hum){
     humidity.append(hum);
-    qv_time.append(time);
     Plot_humidity();
 }
 
-void Dialog:: add_Point_resistance(double time,double res){
+void Dialog:: add_Point_resistance(double res){
     resistance.append(res);
-    qv_time.append(time);
     Plot_resisitance();
-
-
 }
 
-
-
-
-
-
-
-
+void Dialog::add_Point_temperature(double temp){
+     temperature.append(temp);
+     Plot_temperature();
+    }
 
 
 void Dialog::Configure_Port()
 {  if(!is_port_opened){
+    file.open("/home/roshan/Documents/test_test.txt",ios_base::out |ios_base::app);
     foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
         if (serialPortInfo.hasProductIdentifier() && serialPortInfo.hasVendorIdentifier()){
             if (serialPortInfo.productIdentifier()==arduino_product_id && serialPortInfo.vendorIdentifier()==arduino_vendor_id){
@@ -190,8 +208,9 @@ void Dialog::Configure_Port()
         arduino->setParity(QSerialPort::NoParity);
         arduino->setStopBits(QSerialPort::OneStop);
         QObject::connect(arduino,SIGNAL(readyRead()),this,SLOT(readSerial()));
+        timer->start(1000);
         ui->start_pushButton->setText(stop);
-    }else{
+    } else{
         qDebug()<<"port cannot be found";
         QMessageBox::information(this, "serial port error", "cant open serial port to arduino");
 
@@ -205,5 +224,15 @@ else{
         arduino->close();
         is_port_opened=false;
         ui->start_pushButton->setText(start);
+        file.close();
+
+
 }
 }
+
+
+
+
+
+
+
--- Humidity/dialog.h
+++ Humidity/dialog.h
@@ -5,6 +5,15 @@
 #include<QSerialPort>
 #include<qcustomplot.h>
 #include<QtCore>
+#include <QFile>
+#include <QCoreApplication>
+#include <QTextStream>
+#include <iostream>
+#include <fstream>
+#include<QtCore>
+using namespace std;
+
+
 namespace Ui {
 class Dialog;
 }
@@ -18,14 +27,17 @@ public:
     ~Dialog();
 
 public slots:
-   void add_Point_humidity(double time, double hum);
-   void add_Point_resistance(double time, double res);
+   void add_Point_humidity( double hum);
+   void add_Point_resistance(double res);
+   void add_Point_temperature( double temp);
    void Plot_resisitance();
+   void Plot_temperature();
 private slots:
     void readSerial();
-    void updateLCD(QString sensor_reading, QString sensor_reading1);
+    void updateLCD(QString, QString,QString);
     void Plot_humidity();
     void Configure_Port();
+
 
 
 
@@ -37,17 +49,25 @@ private:
     static const qint16 arduino_product_id=67;
   //  static const QString arduino_serialNumber=95530343434351A09022;
     QString serialBuffer;
+    QStringList buffersplit;
     QByteArray serialData;
     qint8 i=1;
+    qint8 j=1;
     int time =0;
-    QString sensor_reading, sensor_reading1;
-    QVector <double> humidity,resistance;
-    QVector <double> qv_time;
+    QString sensor_reading, sensor_reading1,sensor_reading2;
+    QVector <double> humidity,resistance,temperature,qv_time;
     bool arduino_is_available;
     bool is_port_opened;
     QString arduino_portname;
     QString stop= "STOP";
     QString start= "START";
+    fstream file;
+
+
+
+
+
+
 };
 
 #endif // DIALOG_H
